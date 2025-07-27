e978a57330381913bfb21713affd10106edb6536
nourr228 <nour.sobh@student.guc.edu.eg>
2025-07-27 13:28:48 +0300

    finished the delete button, keep the checkbox status

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
+let taskList = [];
+let counter = 1;
+
+function renderTasks() {
+  const list = document.querySelector("#taskList");
+  list.innerHTML = "";
+  taskList.forEach((task, i) => {
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const button = document.createElement("button");
+    button.className = "delete";
+    checkbox.type = "checkbox";
+    checkbox.checked = task.checked;
+    checkbox.addEventListener("change", function () {
+      taskList[i].checked = checkbox.checked;
+      localStorage.setItem("taskArray", JSON.stringify(taskList));
+    });
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(" " + task.name));
+    li.appendChild(button);
+    list.appendChild(li);
+    button.addEventListener("click", function () {
+      deleteTask(task.id);
+    });
+  });
+}
+
 function addTask(event) {
-  const taskItem = document.querySelector("#task").value;
-  if (taskItem) {
-    const item = document.createElement("li");
-    item.innerHTML =
-      "<input type='checkbox' />" +
-      taskItem +
-      "<button class='delete'></button>";
-    document.querySelector("#taskList").append(item);
-    localStorage.setItem(
-      "tasklist",
-      document.querySelector("#taskList").innerHTML
-    );
+  event.preventDefault();
+  let taskItem = {
+    id: counter,
+    name: document.querySelector("#task").value,
+    checked: false,
+  };
+  if (taskItem.name) {
+    taskList.push(taskItem);
+    counter++;
+    localStorage.setItem("taskArray", JSON.stringify(taskList));
+    renderTasks();
+    document.querySelector("#task").value = "";
   } else {
     alert("Please type a task!");
   }
 }
 
+function deleteTask(id) {
+  taskList = taskList.filter((task) => task.id !== id);
+  localStorage.setItem("taskArray", JSON.stringify(taskList));
+  renderTasks();
+}
+
 function deleteAll() {
   localStorage.clear();
-  document.querySelector("#taskList").innerHTML = "";
+  taskList = [];
+  counter = 1;
+  renderTasks();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#taskList").innerHTML =
-    localStorage.getItem("tasklist");
+  taskList = JSON.parse(localStorage.getItem("taskArray")) || [];
+  renderTasks();
 });
